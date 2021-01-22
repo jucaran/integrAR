1062335c42b1a12775fd058303748f49ec61b22e
Franco Ascarate <70088727+francoasc@users.noreply.github.com>
2021-01-22 01:19:10 -0300

    4 screens added for tests

---
+++ client/Screens/CoursesScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CoursesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Cursos</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default CoursesScreen;
