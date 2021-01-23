a1a976c9b52db46ed72521eeec2af70dddbc994b
laupucci <laupuccinelli@gmail.com>
2021-01-23 19:59:05 -0300

    app-superAdmin addCourses, add subject, screen

--- client/App.js
+++ client/App.js
@@ -12,6 +12,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AuthContext, AuthProvider } from "./contexts/AuthProvider";
 import MyTabs from "./Screens/Tab";
 import AuthStack from "./AuthStack";
+import CenterView from "./utils/CenterView";
 
 // Apollo client
 const httpLink = createHttpLink({
@@ -39,37 +40,36 @@ const App = () => {
   const { user, login } = useContext(AuthContext);
   const [loading, setLoading] = useState(true);
 
-  //We check if the user is logged or not
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       token = await AsyncStorage.getItem("token");
-  //       if (token) {
-  //         //here we decode the token
-  //         login();
-  //       } else {
-  //         setLoading(false);
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //       setLoading(false);
-  //     }
-  //   })();
-  // }, []);
+ // We check if the user is logged or not
+  useEffect(() => {
+    (async () => {
+      try {
+        token = await AsyncStorage.getItem("token");
+        if (token) {
+          //here we decode the token
+          login();
+        } else {
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    })();
+  }, []);
 
-  // if (loading)
-  //   return (
-  //     <CenterView>
-  //       <ActivityIndicator />
-  //     </CenterView>
-  //   );
+  if (loading)
+    return (
+      <CenterView>
+        <ActivityIndicator />
+      </CenterView>
+    );
 
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
         <NavigationContainer>
-          {/* {user ? <MyTabs /> : <AuthStack />} */}
-          <AuthStack />
+          {user ? <MyTabs /> : <AuthStack />}
         </NavigationContainer>
       </AuthProvider>
     </ApolloProvider>
