7a61408bbdc62c6e87fdaaf435ccf841b110f1a1
Juanque <juan.castro.arancibia@gmail.com>
2021-01-23 20:18:17 -0300

    fixes merge conflicts

--- client/App.js
+++ client/App.js
@@ -7,16 +7,8 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-<<<<<<< HEAD
-import { NavigationContainer } from "@react-navigation/native";
 import { AuthContext, AuthProvider } from "./contexts/AuthProvider";
-import MyTabs from "./Screens/Tab";
-import AuthStack from "./AuthStack";
 import CenterView from "./utils/CenterView";
-=======
-import { AuthProvider } from "./contexts/AuthProvider";
-import Routes from "./Routes";
->>>>>>> f0349c7d369d45731bc87aa7a1f86e40fd2480a0
 
 // Apollo client
 const httpLink = createHttpLink({
@@ -39,38 +31,6 @@ const client = new ApolloClient({
 });
 
 const App = () => {
-<<<<<<< HEAD
-  const { user, setUser } = useContext(AuthContext);
-  const [loading, setLoading] = useState(true);
-
- // We check if the user is logged or not
-  useEffect(() => {
-    (async () => {
-      try {
-        token = await AsyncStorage.getItem("token");
-        if (token) {
-          const user = jwt.decode(token);
-          setUser(user);
-          setLoading(false);
-        } else {
-          setLoading(false);
-        }
-      } catch (err) {
-        console.log(err);
-        setLoading(false);
-      }
-    })();
-  }, []);
-
-  if (loading)
-    return (
-      <CenterView>
-        <ActivityIndicator />
-      </CenterView>
-    );
-
-=======
->>>>>>> f0349c7d369d45731bc87aa7a1f86e40fd2480a0
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
