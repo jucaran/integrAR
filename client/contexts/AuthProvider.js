a4a6b171f21801daad40dae10acb6a7d7a63eab1
Juanque <juan.castro.arancibia@gmail.com>
2021-01-23 20:58:08 -0300

    adds connection of login with back

--- client/contexts/AuthProvider.js
+++ client/contexts/AuthProvider.js
@@ -1,5 +1,15 @@
 import AsyncStorage from "@react-native-community/async-storage";
 import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+import jwt from "jsonwebtoken";
+
+const LOG_USER = gql`
+  mutation LogUser($dni: String!, $password: String!) {
+    logUser(dni: $dni, password: $password) {
+      token
+    }
+  }
+`;
 
 export const AuthContext = React.createContext({
   user: null,
@@ -11,26 +21,36 @@ export const AuthContext = React.createContext({
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [logUser, { data, error }] = useMutation(LOG_USER);
+
   return (
     <AuthContext.Provider
       value={{
         user,
-        login: async (user) => {
-          console.log("Log in triggered", user);
-          //Here it should be the API call
-          // const newUser = { username: "bob" };
-          // try {
-          //   setUser(user);
-          //   await AsyncStorage.setItem("user", user);
-          // } catch (err) {
-          //   console.log(err);
-          // }
+        login: async ({ dni, password }) => {
+          console.log("Log in triggered", dni, " ", password);
+          console.log("DNI: ", dni);
+          console.log("Password", password);
+          try {
+            await logUser({
+              variables: { dni, password },
+            });
+            if (error) {
+              console.log(error);
+              return false;
+            }
+            const newUser = jwt.decode(data.token);
+            setUser(newUser);
+            await AsyncStorage.setItem("token", data.token);
+          } catch (err) {
+            console.log(err);
+          }
         },
         logout: async () => {
           console.log("Log out triggered");
           try {
             setUser(null);
-            await AsyncStorage.removeItem("user");
+            await AsyncStorage.removeItem("token");
           } catch (err) {
             console.log(err);
           }
