eb8081d9637c3e8182866fbd3b83864e0228ce73
Juanque <juan.castro.arancibia@gmail.com>
2021-01-26 01:16:14 -0300

    fixes course schema

--- api/src/models/Course.js
+++ api/src/models/Course.js
@@ -1,33 +1,39 @@
-import { Schema, model } from "mongoose"
+import { Schema, model } from "mongoose";
 
 // Curso --> 1° A
 const courseSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
-  grade: [{
+  grade: {
     type: Schema.Types.ObjectId,
     ref: "Grade",
     autopopulate: true,
-  }],
-  teachers: [{
-    type: Schema.Types.ObjectId,
-    ref: "Teacher",
-    autopopulate: true
-  }],
-  subjects: [{
-    type: Schema.Types.ObjectId,
-    ref: "Subject",
-    autopopulate: true
-  }],
-  students: [{
-    type: Schema.Types.ObjectId,
-    ref: "Student",
-    autopopulate: true
-  }]
-})
+  },
+  teachers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Teacher",
+      autopopulate: true,
+    },
+  ],
+  subjects: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Subject",
+      autopopulate: true,
+    },
+  ],
+  students: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Student",
+      autopopulate: true,
+    },
+  ],
+});
 
-courseSchema.plugin(require('mongoose-autopopulate'));
+courseSchema.plugin(require("mongoose-autopopulate"));
 
-export default model("Course", courseSchema)
+export default model("Course", courseSchema);
