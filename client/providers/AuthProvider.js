2a00726a5e4866a9f3cdb0269b25e31e78f8770c
Juanque <juan.castro.arancibia@gmail.com>
2021-01-26 17:34:18 -0300

    adds features to Editing screens

--- client/providers/AuthProvider.js
+++ client/providers/AuthProvider.js
@@ -1,11 +1,12 @@
 import AsyncStorage from "@react-native-community/async-storage";
 import React, { useState } from "react";
-import { useMutation, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 
 const LOG_USER = gql`
-  mutation LogUser($dni: String!, $password: String!) {
-    logUser(dni: $dni, password: $password) {
+  {
+    login(dni: $dni, password: $password) {
       token
+      user
     }
   }
 `;
@@ -20,7 +21,7 @@ export const AuthContext = React.createContext({
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [logUser, { data, error }] = useMutation(LOG_USER);
+  const { data, error, loading } = useQuery(LOG_USER);
 
   return (
     <AuthContext.Provider
@@ -30,23 +31,23 @@ export const AuthProvider = ({ children }) => {
           console.log("Log in triggered");
           console.log("DNI: ", dni);
           console.log("Password", password);
-          // try {
-          //   await logUser({
-          //     variables: { dni, password },
-          //   });
-          //   if (error) {
-          //     console.log(error);
-          //     return false;
-          //   }
-          // setUser(data.user);
-          const userEj = { dni, name: "Preceptor", role: "admin" };
-          setUser(userEj);
-          await AsyncStorage.setItem("user", JSON.stringify(userEj));
-          // await AsyncStorage.setItem("token", data.token);
-          // await AsyncStorage.setItem("user", JSON.stringify(data.user));
-          // } catch (err) {
-          //   console.log(err);
-          // }
+          try {
+            await logUser({
+              variables: { dni, password },
+            });
+            if (error) {
+              console.log(error);
+              return false;
+            }
+            setUser(data.user);
+            await AsyncStorage.setItem("token", data.token);
+            await AsyncStorage.setItem("user", JSON.stringify(data.user));
+            // const userEj = { dni, name: "Preceptor", role: "admin" };
+            // setUser(userEj);
+            // await AsyncStorage.setItem("user", JSON.stringify(userEj));
+          } catch (err) {
+            console.log(err);
+          }
         },
         logout: async () => {
           console.log("Log out triggered");
