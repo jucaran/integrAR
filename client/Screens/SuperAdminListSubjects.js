b8a0adff34fa0b360a95116fe299dd404dc0a778
Franco Ascarate <70088727+francoasc@users.noreply.github.com>
2021-01-27 04:47:31 -0300

    SuperAdminListSubjects updated

--- client/Screens/SuperAdminListSubjects.js
+++ client/Screens/SuperAdminListSubjects.js
@@ -1,29 +1,55 @@
 import React from "react";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
+import { ScrollView } from "react-native-gesture-handler";
 import {
   View,
   ActivityIndicator,
   Text,
-  Image,
   TouchableHighlight,
+  Alert,
+  StyleSheet,
 } from "react-native";
 import CenterView from "../utils/CenterView";
-import { gql, useQuery } from "@apollo/client";
-import { Card, ListItem, Button, Icon, Avatar } from "react-native-elements";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { Card } from "react-native-elements";
+import { Button } from "react-native-elements";
 
-const GET_ALL_SUBJECTS = gql`
-  {
-    subjects {
+const GET_SUBJECTS_FROM_COURSE_BY_ID = gql`
+  query GetSubjectsFromCourseId($_id: ID) {
+    courses(_id: $_id) {
+      _id
+      name
+      subjects {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+const DELETE_SUBJECT_BY_ID = gql`
+  mutation DeleteSubject($_id: ID) {
+    deleteSubject(_id: $_id) {
       _id
       name
     }
   }
 `;
 
-const SuperAdminListSubjects = ({ navigation }) => {
-  const { data, loading, error } = useQuery(GET_ALL_SUBJECTS);
-  if (data) {
-    const { subjects } = data;
+const SuperAdminListSubjects = ({ navigation, route }) => {
+  const { _id } = route.params.params;
+  const { data, loading, error } = useQuery(GET_SUBJECTS_FROM_COURSE_BY_ID, {
+    variables: { _id },
+  });
+
+  const [deleteSubject, mutationData] = useMutation(DELETE_SUBJECT_BY_ID);
+  if (error) {
+    return (
+      <CenterView>
+        <Text>ERROR</Text>
+      </CenterView>
+    );
+  } else if (data) {
+    const subjects = data.courses[0].subjects;
 
     return (
       <ScrollView>
@@ -36,13 +62,15 @@ const SuperAdminListSubjects = ({ navigation }) => {
           <TouchableHighlight
             activeOpacity={0.6}
             underlayColor="ligthgrey"
-            onPress={() => navigation.navigate("SuperAdminAddSubject")}
+            onPress={() =>
+              navigation.navigate("SuperAdminAddSubject", {
+                params: data.courses[0]._id,
+              })
+            }
           >
             <Text
               style={{
                 fontSize: 25,
-                // marginBottom: 20,
-                // marginTop: 20,
                 marginLeft: 20,
               }}
             >
@@ -50,53 +78,142 @@ const SuperAdminListSubjects = ({ navigation }) => {
             </Text>
           </TouchableHighlight>
           {subjects.length ? (
-            // <Card>
-            //   <Card.Title>MATERIAS</Card.Title>
-            //   <Card.Divider />
-            //   {subjects.map(({ name, _id }, i) => {
-            //     //_id name __typename
-            //     return (
-            //       <View key={_id}>
-            //         <Text>{name}</Text>
-            //       </View>
-            //     );
-            //   })}
-            // </Card>
-            <Card containerStyle={{ padding: 0 }}>
-              {subjects.map(({ name, _id }, i) => (
-                <ListItem key={i}>
-                  <Avatar
-                    source={{
-                      uri:
-                        "https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png/revision/latest/top-crop/width/360/height/360?cb=20201222215437",
+            <Card>
+              <Card.Title>MATERIAS DE {data.courses[0].name}</Card.Title>
+              <Card.Divider />
+              {subjects.map((subject, i) => {
+                return (
+                  <View
+                    key={subject._id}
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      flexDirection: "row",
+                      margin: 20,
+                      maxWidth: 900,
                     }}
-                  />
-                  <ListItem.Content>
-                    <ListItem.Title>{name}</ListItem.Title>
-                    {/* <ListItem.Subtitle></ListItem.Subtitle> */}
-                  </ListItem.Content>
-                </ListItem>
-              ))}
+                  >
+                    <Text style={{ fontSize: 18 }}>{subject.name}</Text>
+                    <Button
+                      title="Eliminar Materia"
+                      onPress={() =>
+                        Alert.alert(
+                          "Eliminar curso",
+                          `¿Está seguro que desea eliminar esta materia:
+    "${subject.name}"?`,
+                          [
+                            {
+                              text: "Cancelar",
+                              style: "cancel",
+                            },
+                            {
+                              text: "OK",
+                              onPress: async () =>
+                                await deleteSubject({
+                                  variables: { _id: subject._id },
+                                  refetchQueries: [
+                                    { query: GET_SUBJECTS_FROM_COURSE_BY_ID },
+                                  ],
+                                }),
+                            },
+                          ]
+                        )
+                      }
+                    />
+                  </View>
+                );
+              })}
             </Card>
           ) : (
             <CenterView>
-              <Text>ERROR, NO HAY MATERIAS</Text>
+              <Text>NO HAY MATERIAS PARA ESTE CURSO</Text>
             </CenterView>
           )}
         </View>
       </ScrollView>
     );
-  } else if (error)
-    return (
-      <View>
-        <Text>ERROR</Text>
-      </View>
-    );
-  else
+  } else
     return (
       <CenterView>
         <ActivityIndicator size="large" />
       </CenterView>
     );
 };
+const styles = StyleSheet.create({
+  cont: {
+    flex: 1,
+    padding: 5,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  touchText: {
+    marginTop: 5,
+    marginBottom: 15,
+    // fontFamily: "roboto",
+    fontSize: 16,
+    alignItems: "flex-start",
+    color: "#2290CD",
+  },
+  touch: {
+    justifyContent: "flex-start",
+    marginLeft: 12,
+  },
+  card: {
+    margin: 5,
+    backgroundColor: "#00aadd",
+    borderRadius: 10,
+    padding: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardText: {
+    fontSize: 20,
+    padding: 10,
+    color: "white",
+  },
+  cardText: {
+    fontSize: 20,
+    padding: 10,
+    color: "white",
+  },
+  img: {
+    color: "white",
+    fontSize: 15,
+  },
+  cardIn: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 344,
+  },
+});
+
+// <TouchableHighlight
+// activeOpacity={0.6}
+// underlayColor="ligthgrey"
+// onPress={() =>
+//   Alert.alert(
+//     "Eliminar curso",
+//     `¿Está seguro que desea eliminar esta materia:
+//     "${subject.name}"?`,
+//     [
+//       {
+//         text: "Cancelar",
+//         style: "cancel",
+//       },
+//       {
+//         text: "OK",
+//         onPress: () => console.log("holis"),
+//       },
+//     ]
+//   )
+// }
+// >
+// <Button title="Eliminar Materia" />
+// </TouchableHighlight>
 export default SuperAdminListSubjects;
