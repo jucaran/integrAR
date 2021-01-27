16ecd377070d825ebbbf1a2b04f1841d740b8abe
laupucci <laupuccinelli@gmail.com>
2021-01-26 22:13:26 -0300

    navigate

--- client/Screens/SuperAdminScreen.js
+++ client/Screens/SuperAdminScreen.js
@@ -57,9 +57,7 @@ const SuperAdminView = ({ navigation }) => {
           activeOpacity={0.6}
           underlayColor="ligthgrey"
           style={styles.touch}
-          onPress={() => navigation.navigate("Students", {
-            screen: "ListStudents"
-          })}
+          onPress={() => navigation.navigate("SuperAdminListStudents")}
         >
           <View style={styles.button}>
             <Image source={require("../assets/alumn.png")} style={styles.img} />
