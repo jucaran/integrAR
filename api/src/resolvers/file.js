ee8a90f07446622f5c9dd40b9a5ef892da69be87
Juanque <juan.castro.arancibia@gmail.com>
2021-01-29 13:17:59 -0300

    begins to parse de csv file in the back

--- api/src/resolvers/file.js
+++ api/src/resolvers/file.js
@@ -2,18 +2,23 @@ export const uploadCsv = async (_, { file }) => {
   // console.log(file);
   const { createReadStream, name, mimetype, encoding } = await file;
   const stream = createReadStream();
-  let fileString = [];
+  let fileBuffer = [];
 
   stream.on("data", function (chunk) {
-    fileString = [...fileString, chunk];
-    console.log("chunk", chunk);
+    fileBuffer = [...fileBuffer, chunk];
   });
 
   stream.on("end", function () {
-    console.log("File String: ", Buffer.concat(fileString));
+    const rows = Buffer.concat(fileBuffer).toString("utf8").split("\r\n");
+
+    const fields = rows.map((el) => el.split(","));
+
+    console.log(fields);
+
+    return {
+      status: true,
+    };
   });
 
-  return {
-    status: true,
-  };
+  console.log("cheking if async");
 };
