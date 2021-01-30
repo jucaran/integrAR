475a8ee732978eeeaa0c8d34cfaf305bbd16ec98
laupucci <laupuccinelli@gmail.com>
2021-01-30 05:46:39 -0300

    addteachertosubject

--- client/screens/SuperAdmin/AddTeacherToSubject.js
+++ client/screens/SuperAdmin/AddTeacherToSubject.js
@@ -31,68 +31,62 @@ const GET_ALL_TEACHERS = gql`
   }
 `;
 
-//  const EDIT_SUBJECT = gql`
-//   mutation EditSubject(
-//       $_id: ID!,
-//       $teacher: 
-//       $_id: ID!
-//   ) {
-//     editSubject(
-//         _id: $_id
-//       input: {
-//         teacher: { 
-//             _id: $_id}
-//         }
-//     ) {
-//         name
-//         teacher {
-//             _id
-//         name
-//       }
-//     }
-//   }
-// `;
+ const EDIT_SUBJECT = gql`
+  mutation EditSubject(
+      $_id: ID!
+      $teacher: ID!
+  ) {
+    editSubject(
+        _id: $_id
+      input: {
+        teacher: $teacher
+        }
+    ) {
+        name
+        teacher {
+            _id
+        name
+      }
+    }
+  }
+`;
 
 
 
 export default function AddTeacherToSubject ({ navigation, route }) {
   const id = route.params.params.id
   const { data, loading, error } = useQuery(GET_ALL_TEACHERS);
-  console.log('soy id', id)
+  const [editSubject, {mutationData, mutationLoading, mutationError }] = useMutation(EDIT_SUBJECT);
 
   const handleOnPress = async (teacherId, id, name, lastname) => {
-    console.log('soy id', id)
-    console.log('soy teacherid', teacherId)
-    console.log('soy teacher', name, lastname)
-    // try {
-    //   await editSubject({
-    //     variables: {
-    //       _id: id,
-    //       teacher: {
-    //       _id: teacherId
-    //       }
-    //     },
-    //     refetchQueries: [{ query: GET_SUBJECTS_FROM_COURSE_BY_ID }],
-    //   });
-    //   navigation.navigate("SuperAdminListSubject", {
-    //     params: id,
-    //   })
-    //   if (error) {
-    //     console.log(error);
-    //     return false;
-    //   }
-    //   return alert(
-    //     `El profesor ${name} ${lastname} fue agregado exitosamente!`
-    //   );
-    // } catch (err) {
-    //   console.error("soy el catch", err);
-    // }
+    try {
+      await editSubject({
+        variables: {
+          _id: id,
+          teacher: teacherId
+        },
+        // refetchQueries: [{ query: GET_SUBJECTS_FROM_COURSE_BY_ID }],
+      });
+      navigation.navigate("GradeScreen")
+      //   "SuperAdminListSubject",{
+      //   screen: "SuperAdminListSubject",
+      //   params: id,
+      // })
+      if (mutationError) {
+        console.log(mutationError);
+        return false;
+      }
+      return alert(
+        `El profesor ${name} ${lastname} fue agregado exitosamente!`
+      );
+    } catch (err) {
+      console.error("soy el catch", err);
+    }
   };
 
 
 
-  if (loading)
-  //  || mutationData.loading)
+  if (loading || mutationLoading)
   { return (
     <CenterView>
         <ActivityIndicator />
@@ -100,59 +94,36 @@ export default function AddTeacherToSubject ({ navigation, route }) {
     );}
     
     if (data) {
-      const { teachers, courses, subjects } = data;
+      const { teachers } = data;
       return (
         
       <ScrollView>
         <CenterView>
         <View style={styles.principal}>
+        <Card>
+          <Card.Title>Profesor para</Card.Title>
+          <Card.Divider />
         {teachers.map((teacher, i) =>{
          // {isTeacher=false}
           return(
-          <View key={teacher._id}>
-            <Text>{teacher.name} {teacher.lastname}</Text>
-            {console.log('soy id', id)}
-    {console.log('soy teacherid', teacher._id)}
-    {console.log('soy teacher', teacher.name, teacher.lastname)}
+            <Card key={teacher._id} style={styles.card}>
+            <Text style={styles.prof}>{teacher.name} {teacher.lastname}</Text>
             <TouchableHighlight
-            onPress={handleOnPress(teacher._id, id, teacher.name, teacher.lastname)}
+            style={styles.onPress}
+            onPress={()=>handleOnPress(teacher._id, id,
+              teacher.name, teacher.lastname)}
             >
-              <Text>Agregar</Text>
+              <Text style={styles.cardT}>Agregar</Text>
             </TouchableHighlight>
-          </View>
+          </Card>
           )
         })}
-            
-          {/* <FlatList
-          data={teachers}
-          extraData={isTeacher}
-          renderItem={({ item: teacher }) => {
-            isTeacher=false
-            return (
-              <Card key={teacher._id} style={styles.card}>
-                  {console.log(isTeacher)}
-                  <View style={styles.cardcont}>
-                    <View style={styles.prof}>
-                      <Text style={styles.name}>
-                        {teacher.name} {teacher.lastname}
-                      </Text>
-                      <Switch style={styles.switch}
-                      trackColor={{ false: "#767577", true: "#2290CD" }}
-                      thumbColor={isTeacher ? "#8FC6E4" : "#f4f3f4"}
-                      value={isTeacher}
-                      onValueChange={() =>setIsTeacher(true)}/>
-                     </View>
-                    </View>
-                </Card>
-              );
-            }}
-            keyExtractor={({ _id }) => _id}
-          /> */}
+        </Card>
         </View>
         </CenterView>
       </ScrollView>
     );
-  } else if (error)//  || mutationData.error)
+  } else if (error  || mutationError)
    { return (
       <View>
         <Text>ERROR</Text>
@@ -171,49 +142,56 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     flexDirection: "column",
   },
-  cardcont: {
-    display: "flex",
-    flexDirection: "column",
-  },
+  // cardcont: {
+  //   display: "flex",
+  //   flexDirection: "column",
+  // },
   prof: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginRight: 14,
+    fontSize: 17,
+   },
+  // img: {
+  //   width: 14,
+  //   height: 14,
+  //   marginTop: 10,
+  //   marginRight: 25,
+  // },
+  // name: {
+  //   fontSize: 16,
+  //   width: 280,
+  //   // fontFamily: "roboto",
+  //   color: "#000000",
+  //   marginLeft: 10,
+  //   fontWeight: "bold",
+  // },
+  // desc: {
+  //   flexDirection: "row",
+  // },
+  // description: {
+  //   fontSize: 14,
+  //   // fontFamily: "roboto",
+  //   color: "#000000",
+  //   marginLeft: 10,
+  // },
+  // touchText: {
+  //   marginTop: 15,
+  //   marginBottom: 15,
+  //   // fontFamily: "roboto",
+  //   fontSize: 14,
+  //   alignItems: "flex-start",
+  //   color: "#2290CD",
+  // },
+  // touch: {
+  //   justifyContent: "flex-start",
+  //   margin: 5,
+  //   marginLeft: 12,
+  // },
+  onPress: {
+    backgroundColor: "#2290CD",
+    padding: 7,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 2
   },
-  img: {
-    width: 14,
-    height: 14,
-    marginTop: 10,
-    marginRight: 25,
-  },
-  name: {
-    fontSize: 16,
-    width: 280,
-    // fontFamily: "roboto",
-    color: "#000000",
-    marginLeft: 10,
-    fontWeight: "bold",
-  },
-  desc: {
-    flexDirection: "row",
-  },
-  description: {
-    fontSize: 14,
-    // fontFamily: "roboto",
-    color: "#000000",
-    marginLeft: 10,
-  },
-  touchText: {
-    marginTop: 15,
-    marginBottom: 15,
-    // fontFamily: "roboto",
-    fontSize: 14,
-    alignItems: "flex-start",
-    color: "#2290CD",
-  },
-  touch: {
-    justifyContent: "flex-start",
-    margin: 5,
-    marginLeft: 12,
-  },
+  cardT: {
+  color: "white",}
 });
