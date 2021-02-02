abc7723b4404018cf6eae47f1b4bd968c6f1e873
sacunap <sixto.acunap@gmail.com>
2021-02-02 16:23:16 -0300

    Fixed some imports

--- api/src/schemas/module.js
+++ api/src/schemas/module.js
@@ -2,6 +2,7 @@ import { gql } from "apollo-server-express";
 
 export const moduleTypes = gql`
   type Module {
+    _id: ID
     name: String
     classes: [Class]
     test: String
--- client/screens/Teacher/TeacherScreen.js
+++ client/screens/Teacher/TeacherScreen.js
@@ -17,7 +17,7 @@ const TeacherView = ({ navigation }) => {
         <View style={styles.materias}>
           <TouchableHighlight
             style={styles.touch}
-            activeOpacity={0.6}
+            //activeOpacity={0.6}
             onPress={() =>
               navigation.navigate("Materias", {
                 screen:"TeacherListSubjects",
@@ -36,7 +36,7 @@ const TeacherView = ({ navigation }) => {
         <View style={styles.alumn}>
           <TouchableHighlight
             style={styles.touch}
-            activeOpacity={0.6}
+            //activeOpacity={0.6}
             onPress={() =>
               navigation.navigate("Cursos", {
                 screen:"TeacherListCourses",
